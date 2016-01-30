5f81b19f9455683ba671620a83f09791c52b3e6a
kyle <altck8@gmail.com>
2016-01-29 16:34:44 -0800

    fixed specs to use stylist_id instead of stylist name

--- spec/salon_spec.rb
+++ spec/salon_spec.rb
@@ -41,20 +41,20 @@ end
 describe Client do
   describe "#new" do
     it 'creates a new stylist, with name' do
-      new_client = Client.new( {name: "Matt", stylist: "kyle", id: 0} )
+      new_client = Client.new( {name: "Matt", stylist_id: -1, id: 0} )
       expect(new_client.name).to(eq("Matt"))
     end
     it 'creates a new stylist, with id of Fixnum' do
-      new_client = Client.new( {name: "Matt", stylist: "kyle", id: 1} )
+      new_client = Client.new( {name: "Matt", stylist_id: -1, id: 1} )
       expect(new_client.id).to(be_instance_of(Fixnum))
     end
     it 'adds client to the db' do
-      new_client = Client.new( {name: "kyle", stylist: "matt", id: nil } )
+      new_client = Client.new( {name: "kyle", stylist_id: -1, id: nil } )
       DB_TEST_SALON.exec("INSERT INTO clients (name) VALUES ('#{new_client.name}');")
       found = DB_TEST_SALON.exec("SELECT * FROM clients;")
       result_list = []
       found.each do |client|
-        result_list.push( Client.new( {name: client.fetch("name"), stylist: "matt", id: client.fetch("id")} ) )
+        result_list.push( Client.new( {name: client.fetch("name"), stylist_id: -1, id: client.fetch("id")} ) )
       end
       expect(result_list[0].name).to(eq(new_client.name))
     end
